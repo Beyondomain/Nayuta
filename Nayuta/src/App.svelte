c9e1c35653dd228566de979f305eaffde297d788
SimSimhan <simsimhan73@gmail.com>
2023-11-27 01:40:09 +0900

    asads

---
+++ Nayuta/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+</script>
+
+
+<main>
+  <h1>Nayuta</h1>
+  <div class="main">
+    <input/>
+    <button>Search</button>
+  </div>
+  <footer>made by simsimhan and akiacode</footer>
+</main> 
